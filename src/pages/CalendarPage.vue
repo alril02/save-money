857868ced22f1905cbbc2737a0fe3baf3b8afc1e
alril02 <131113396+alril02@users.noreply.github.com>
2025-10-21 15:51:01 +0800

    tambah isi calendar

--- src/pages/CalendarPage.vue
+++ src/pages/CalendarPage.vue
@@ -1,6 +1,7 @@
 <template>
     <div>
         <h1>Halaman Calendar</h1>
+        <div>isi calendar</div>
     </div>
 </template>
 <script setup></script>
--- src/pages/login/LoginPage.vue
+++ src/pages/login/LoginPage.vue
@@ -1,6 +1,7 @@
 <template>
   <q-page>
     <h3>Halaman login</h3>
+    <div>form login</div>
   </q-page>
 </template>
 <script></script>
