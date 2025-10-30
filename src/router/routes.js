3d1c450b94aa56258b352a7c2308cb2acd4b58d1
nikitarondonuwu03-stack <nikitarondonuwu03@gmail.com>
2025-10-29 22:59:54 -0700

    buat LoginPage dan SignUpPage
    buat form Login dan SignUp

--- src/router/routes.js
+++ src/router/routes.js
@@ -3,16 +3,28 @@ const routes = [
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
     children: [
-      { path: '', component: () => import('pages/DashboardPage.vue') }],
-      { path: 'squad', component: () => import('pages/SquadPage.vue') }],
+      { path: '', component: () => import('pages/DashboardPage.vue') },
+      { path: 'squad', component: () => import('pages/SquadPage.vue') },
       { path: 'messenger', component: () => import('pages/MessengerPage.vue') },
       { path: 'statistic', component: () => import('pages/StatisticPage.vue') },
       { path: 'calendar', component: () => import('pages/CalendarPage.vue') },
-      { path: 'finance', component: () => import('pages/FinancePage.vue') }
-  }, 
+      { path: 'finance', component: () => import('pages/FinancePage.vue') },
+    ],
+  },
 
-  // Always leave this as last one,
-  // but you can also remove it
+  {
+    path: '/login',
+    component: () => import('layouts/LoginLayout.vue'),
+    children: [{ path: '', component: () => import('pages/Login/LoginPage.vue') }],
+  },
+
+  {
+    path: '/signup',
+    component: () => import('layouts/LoginLayout.vue'),
+    children: [{ path: '', component: () => import('pages/Login/SignUpPage.vue') }],
+  },
+
+  // Halaman Error 404
   {
     path: '/:catchAll(.*)*',
     component: () => import('pages/ErrorNotFound.vue'),
